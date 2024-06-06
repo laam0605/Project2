bfad4a75ef2a395772a61a07f95cc18122ec2ae9
unknown <ntg1611@gmail.com>
2024-06-06 22:59:47 +0700

    update etc...

--- resources/views/client/checkout.blade.php
+++ resources/views/client/checkout.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>STORYBOUND | Cart</title>
+    <title>BookStore | Checkout</title>
     <link rel="icon" href="img/book-logo.png" type="image">
     <link rel="stylesheet" href="/css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
